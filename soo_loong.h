e02c7ccc4561d4d8f10d0b3c06cc03c203cc5ad5
Maeva ALBRAND <malbrand@e2r8p11.clusters.42paris.fr>
2021-11-26 07:43:01 +0100

    norme

--- soo_loong.h
+++ soo_loong.h
@@ -1,8 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   soo_loong.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malbrand <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/26 07:37:14 by malbrand          #+#    #+#             */
+/*   Updated: 2021/11/26 07:42:29 by malbrand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SOO_LOONG_H
 # define SOO_LOONG_H
-#endif
-# ifndef BUFFER_SIZE
-# define BUFFER_SIZE 6
 
 # include <sys/types.h>
 # include <sys/stat.h>
@@ -11,8 +20,11 @@
 # include <stdlib.h>
 # include <stdio.h>
 //# include <mlx.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 6
+# endif
 
-typedef struct 	s_map
+typedef struct s_map
 {
 	int	nlig;
 	int	ncol;
@@ -22,20 +34,21 @@ typedef struct 	s_map
 	int	exit;
 	int	player;
 	int	**tab;
-}				t_map;
+}					t_map;
 
-size_t	ft_strlen(const char *s);
+int		ft_check(const char *str);
+int		ft_read(int *ret, int fd, char **buff);
+int		ft_strlen_bis(char *s);
 char	*ft_strdup(const char *s);
 char	*ft_substr(const char *s, unsigned int start, size_t len);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char *s1, const char *s2);
 char	*ft_fill_line(char **str, char *line);
 char	*get_next_line(int fd);
-int		ft_check(const char *str);
-int		ft_read(int *ret, int fd, char **buff);
-int		ft_strlen_bis(char *s);
-t_map	ft_error(int fd, char *av);
 void	ft_free_tab(t_map map);
 void	ft_print_tab(t_map map);
 t_map	ft_fill_tab(t_map map, int fd);
+t_map	ft_error(int fd, char *av);
+size_t	ft_strlen(const char *s);
+
 #endif
